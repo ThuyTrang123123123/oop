53ba38aec8635741b52c4ab326848f0bf8130edb
Thuỳ Tranng <thuytranguete@gmail.com>
2025-09-24 23:30:45 +0700

    ADD HW1

---
+++ HW1/1.2.30/src/Stats5.java
@@ -0,0 +1,23 @@
+public class Stats5 {
+    public static void main(String[] args) {
+        double x1 = Math.random();
+        double x2 = Math.random();
+        double x3 = Math.random();
+        double x4 = Math.random();
+        double x5 = Math.random();
+
+        double min = Math.min(x1, Math.min(x2, Math.min(x3, Math.min(x4, x5))));
+        double max = Math.max(x1, Math.max(x2, Math.max(x3, Math.max(x4, x5))));
+        double average = (x1 + x2 + x3 + x4 + x5) / 5;
+
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(x3);
+        System.out.println(x4);
+        System.out.println(x5);
+
+        System.out.println(average);
+        System.out.println(min);
+        System.out.println(max);
+    }
+}
---
+++ HW1/B/src/Introduction.java
@@ -0,0 +1,17 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("My name: Nguyễn Thùy Trang");
+        System.out.println("ID: 24021644");
+        System.out.println("CLass: K69I-CS3");
+        System.out.println("User name: ThuyTrang123123123");
+        System.out.println("Email: 24021644@vnu.edu.vn");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.\n" +
+                "Take one down, pass it around, \n" +
+                "8 bottles of beer on the wall, 8 bottles of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "...\n" +
+                "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "No more bottles of beer on the wall.");
+    }
+}
